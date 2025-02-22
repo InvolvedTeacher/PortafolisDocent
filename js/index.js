59e8902a589ae95de9abce6269aa0851647927cb
DGMC117 <dgarciadm@gmail.com>
2025-02-22 21:25:50 +0100

    style (navigation): changed navbar style. changed website to one-page site.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
-function showSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+function navigate(page) {
+    let container = document.getElementById("content");
+
+    fetch(page)
+        .then(response => response.text())
+        .then(html => {
+            container.innerHTML = html;
+            let sideboardActive = document.getElementById("sidebar-active")
+            sideboardActive.checked = false;
+        })
+        .catch(error => {
+            console.error("Error fetching page: ", error);
+        }
+        );
 }
 
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-}
+navigate("home");
+
+document.querySelectorAll(".pageLink").forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        let page = this.getAttribute("href").substring(1);
+
+        navigate(page);
+    });
+});
